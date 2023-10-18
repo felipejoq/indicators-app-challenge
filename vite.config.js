1f929af10df0ee7f92cdeff441cb83990ae0deff
felipejoq <felipejofreq@gmail.com>
2023-10-17 22:08:22 -0300

    Config to the deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/indicators-app-challenge/",
+  build: {
+    outDir: "./docs",
+  },
   server: {
     host: true
   }
